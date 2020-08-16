816d13b95704ca642273f3e44adde923f50bfff7
unknown <mdazharulhoque7@gmail.com>
2020-08-16 23:33:35 +0600

    Database Relationship|One to Many & Many to Many

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Customer, Product, Order
+from .models import (Customer,
+                     Tag,
+                     Product,
+                     Order)
 
 admin.site.register(Customer)
+admin.site.register(Tag)
 admin.site.register(Product)
 admin.site.register(Order)
