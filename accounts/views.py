0ea4835388b9dcbe71e982c7bcbae19090ac9bba
unknown <azharul.hoque@televersant.com>
2020-08-19 10:29:12 +0600

    Dynamic URL Routing & Templates(Part 1)

--- accounts/views.py
+++ accounts/views.py
@@ -13,18 +13,25 @@ def home(request):
     order_delivered = orders.filter(status='Delivered').count()
     order_pending = orders.filter(status='Pending').count()
 
-    return render(request, 'accounts/dashboard.html',
-                  {'customers': customers,
+    context = {'customers': customers,
                    'orders':orders,
                    'total_customer':total_customer,
                    'total_orders':total_orders,
                    'order_delivered':order_delivered,
                    'order_pending':order_pending,
-                   })
+                   }
+    return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
     products = Product.objects.all()
     return render(request, 'accounts/products.html', {'products': products})
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, id):
+    customer = Customer.objects.get(id=id)
+    orders = customer.order_set.all()
+    order_count = orders.count()
+    context = {'customer': customer,
+               'orders': orders,
+               'order_count': order_count,
+               }
+    return render(request, 'accounts/customer.html', context)
