f5e0153329f3361f9b9e19bbe9fbcd05ccf0d106
unknown <azharul.hoque@televersant.com>
2020-08-24 17:54:08 +0600

    User Profile With One To One Relationship

--- accounts/views.py
+++ accounts/views.py
@@ -26,6 +26,10 @@ def registerPage(request):
            username = form.cleaned_data.get('username')
            group = Group.objects.get(name='customer')
            user.groups.add(group)
+           
+           Customer.objects.create(
+               user = user
+           )
 
            messages.success(request, 'Account was created for '+username)
            return redirect('login')
@@ -75,8 +79,19 @@ def home(request):
                    }
     return render(request, 'accounts/dashboard.html', context)
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
-    context = {}
+    orders = request.user.customer.order_set.all()
+    total_orders = orders.count()
+    order_delivered = orders.filter(status='Delivered').count()
+    order_pending = orders.filter(status='Pending').count()
+
+    context = {'orders': orders,
+               'total_orders':total_orders,
+               'order_delivered':order_delivered,
+               'order_pending':order_pending,
+               }
     return render(request, 'accounts/user.html', context)
 
 @login_required(login_url='login')
