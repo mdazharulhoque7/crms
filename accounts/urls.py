b200ed7e9679d91fbdb7e4cdf9767dd3f89934cb
unknown <azharul.hoque@televersant.com>
2020-08-26 19:31:06 +0600

    Password Reset Email

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -17,6 +18,26 @@ urlpatterns = [
     path('create_order/<str:customer_id>/', views.createOrder, name='create_order'),
     path('update_order/<str:id>/', views.updateOrder, name='update_order'),
     path('delete_order/<str:id>/', views.deleteOrder, name='delete_order'),
+
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(
+             template_name = 'accounts/password_reset.html'
+         ),name='reset_password'),
+
+    path('reset_password_sent/',
+         auth_views.PasswordChangeDoneView.as_view(
+             template_name = 'accounts/password_reset_sent.html'
+         ), name = 'password_reset_done'),
+
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name = 'accounts/password_reset_form.html'
+         ), name = 'password_reset_confirm'),
+
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name = 'accounts/password_reset_done.html'
+         ), name = 'password_reset_complete'),
 ]
 
 
